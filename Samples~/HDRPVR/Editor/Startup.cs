0a9f367ae2abc4e53bc52ba74d5b810f8d4cb646
Vasanth Mohan <vmohan7@stanford.edu>
2021-07-12 07:00:14 -0700

    Add Comments

--- Samples~/HDRPVR/Editor/Startup.cs
+++ Samples~/HDRPVR/Editor/Startup.cs
@@ -5,8 +5,10 @@ using UnityEditor.PackageManager;
 [InitializeOnLoad]
 public class Startup {
 
+	//Only Load Once
     private static bool loaded = false;
 
+    //On Startup Load the HD Render Pipeline
     static Startup() {
         if (!loaded) {
             Debug.Log("Loading High Definiton Render Pipeline Package from Startup script...");
