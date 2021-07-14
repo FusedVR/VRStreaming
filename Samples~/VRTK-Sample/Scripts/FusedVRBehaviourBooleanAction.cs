bc0f6bb3149316cd4fe7a33323f00123212648be
Vasanth Mohan <vmohan7@stanford.edu>
2021-07-13 20:59:11 -0700

    VRTK Sample Init Commit

---
+++ Samples~/VRTK-Sample/Scripts/FusedVRBehaviourBooleanAction.cs
@@ -0,0 +1,24 @@
+﻿namespace FusedVR.VRStreaming.VRTK
+{
+    using UnityEngine;
+    using Zinnia.Action;
+    using static FusedVR.VRStreaming.ControllerInputManager;
+
+    /// <summary>
+    /// Listens for the linked boolean behavior and emits the appropriate action.
+    /// </summary>
+    public class FusedVRBehaviourBooleanAction : BooleanAction
+    {
+        [Tooltip("Which hand should this behavior listen to") ]
+        public Hand myHand;
+
+        [Tooltip("Which button should this behavior listen to")]
+        public Button myButton;
+
+        public void OnRemoteUpdate(Hand hand, Button button, bool pressed, bool touched) {
+            if (hand == myHand && button == myButton) {
+                Receive(pressed); // if my hand & button is pressed, then send data to VRTK
+            }
+        }
+    }
+}
