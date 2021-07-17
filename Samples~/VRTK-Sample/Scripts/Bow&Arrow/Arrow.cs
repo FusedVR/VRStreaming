74ea3f1196eeae867f37f50562807a3de23465e0
FusedVR <vmohan2211@gmail.com>
2021-07-17 11:42:49 -0700

    Working Bow & Arrow

---
+++ Samples~/VRTK-Sample/Scripts/Bow&Arrow/Arrow.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zinnia.Tracking.Collision;
+
+public class Arrow : MonoBehaviour
+{
+    private bool isFired = false;
+
+    private Rigidbody myPhys;
+
+    public void SetDelayedFired(float sec)
+    {
+        StartCoroutine(SetFired(sec));
+    }
+
+    IEnumerator SetFired(float sec)
+    {
+        yield return new WaitForSeconds(sec);
+        isFired = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myPhys = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (isFired)
+        {
+            transform.LookAt ( transform.position + myPhys.velocity  , Vector3.up);
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (isFired)
+        {
+            Debug.LogError(collision.contacts[0].otherCollider.gameObject.name);
+        }
+    }
+}
---
+++ Samples~/VRTK-Sample/Scripts/Bow&Arrow/FollowOnSnap.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Tilia.Interactions.Interactables.Interactables;
+using Tilia.Interactions.Interactables.Interactors;
+using Tilia.Interactions.SnapZone;
+using UnityEngine;
+
+public class FollowOnSnap : MonoBehaviour
+{
+    public SnapZoneFacade facade;
+
+    private InteractorFacade hand;
+    private int snappedState = 0;
+    private GameObject prevArrow;
+
+    private Transform bowString;
+    private Vector3 stringStartPos;
+    private bool nockable = true;
+
+    private void Start()
+    {
+        bowString = transform.parent;
+        stringStartPos = transform.parent.localPosition;
+    }
+
+    public void OnEnterSnap(GameObject arrow)
+    {
+        if (nockable)
+        {
+            InteractableFacade interactable = arrow.GetComponent<InteractableFacade>(); 
+            hand = interactable.GrabbingInteractors[0];
+            interactable.LastUngrabbed.AddListener(UnGrabbed);
+            facade.Snap(arrow);
+
+            nockable = false;
+        }
+    }
+
+    private void UnGrabbed(InteractorFacade arg0)
+    {
+        facade.Unsnap(); 
+    }
+
+    public void OnSnap(GameObject arrow)
+    {
+        snappedState = 1;
+    }
+
+    public void OnRelease(GameObject arrow)
+    {
+        arrow.GetComponent<InteractableFacade>().LastUngrabbed.RemoveListener(UnGrabbed);
+        hand = null;
+
+        prevArrow = arrow;
+        snappedState = 2;
+        StartCoroutine(DisableNockTill(.1f));
+
+    }
+
+    private IEnumerator DisableNockTill(float secs)
+    {
+        nockable = false;
+        prevArrow.GetComponent<Arrow>().SetDelayedFired(secs);
+
+        yield return new WaitForSeconds(secs);
+
+        nockable = true;
+    }
+
+    private void Update()
+    {
+        switch (snappedState)
+        {
+            case 0:
+                bowString.localPosition = stringStartPos;
+                break;
+            case 1:
+                bowString.localPosition = stringStartPos + 
+                    Mathf.Min ( (Vector3.Distance(hand.transform.position, bowString.parent.position) ) , .45f) * -Vector3.up;
+                break;
+            case 2:
+
+                facade.Unsnap(); // unsnapping again because the SnapZone caught the arrow after the first ungrab
+
+                Rigidbody r = prevArrow.GetComponent<Rigidbody>(); //janky because after release kinimatic gets set for some reasdon
+                r.isKinematic = false;
+                r.velocity = prevArrow.transform.forward * GetPower() * 25f;
+
+                snappedState = 0;
+                break;
+        }
+    }
+
+    private float GetPower()
+    {
+        return Vector3.Distance(bowString.localPosition, stringStartPos); //can convert to a 0-1 Range based on max
+    }
+}
