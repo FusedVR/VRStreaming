2a48dcdae87cd91894180c26d14179a9af1d4153
Vasanth Mohan <vmohan7@stanford.edu>
2021-08-09 07:29:41 -0700

    Save via component

--- Runtime/Scripts/VRBroadcast.cs
+++ Runtime/Scripts/VRBroadcast.cs
@@ -12,6 +12,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using Unity.RenderStreaming;
+using Newtonsoft.Json;
 
 namespace FusedVR.VRStreaming {
     /// <summary>
@@ -20,6 +21,14 @@ namespace FusedVR.VRStreaming {
     /// </summary>
     public class VRBroadcast : SignalingHandlerBase,
         IOfferHandler, IAddChannelHandler, IDisconnectHandler, IDeletedConnectionHandler {
+
+        #region Variables
+        /// <summary>
+        /// Check if client is listening for the same GameID
+        /// </summary>
+        [SerializeField]
+        [Tooltip("This ID should match the input from the Web Browser. Leave blank to match against anything")]
+        private string gameID = "";
 
         /// <summary>
         /// Streams (video, audio, data) that need to be sent to the client
@@ -31,6 +40,7 @@ namespace FusedVR.VRStreaming {
         /// List of all connectionIds that are connected
         /// </summary>
         private List<string> connectionIds = new List<string>();
+        #endregion
 
         #region Disconnect
         public void OnDeletedConnection(SignalingEventData eventData) {
@@ -63,13 +73,20 @@ namespace FusedVR.VRStreaming {
         /// Determines whether to accept offer and if so that to apply sources and submit answer
         /// </summary>
         public void OnOffer(SignalingEventData data) {
+
+            string inputGID = GetGameID(data.sdp);
+            if ( gameID.Length != 0 && inputGID != gameID) {
+                Debug.Log($"Offer Doesn't Match My GameID : {inputGID}");
+                return;
+            }
+
             if (connectionIds.Count >= 1) { //if there is more than 1 connection, let's skip this offer
-                Debug.Log($"Already answered this connectionId : {connectionIds[0]}");
+                Debug.LogWarning($"Already answered this connectionId : {connectionIds[0]}");
                 return;
             }
 
             if (connectionIds.Contains(data.connectionId)) { //if connection is already connected, skip offer
-                Debug.Log($"Already answered this connectionId : {data.connectionId}");
+                Debug.LogWarning($"Already answered this connectionId : {data.connectionId}");
                 return;
             }
             connectionIds.Add(data.connectionId); //confirm we will use this connection
@@ -92,6 +109,14 @@ namespace FusedVR.VRStreaming {
             var channel = streams.OfType<IDataChannel>().
                 FirstOrDefault(r => r.Channel == null && !r.IsLocal);
             channel?.SetChannel(data.connectionId, data.channel);
+        }
+
+        private string GetGameID(string sdp) {
+            string[] parameters = sdp.Split('\n'); //split on new line
+            string customParam = parameters[parameters.Length - 2].Split('=')[1]; //get line before last new line and split on =
+            string value = ""; //get dictionary value
+            JsonConvert.DeserializeObject<Dictionary<string, string>>(customParam).TryGetValue("user", out value);
+            return value;
         }
     }
 }
