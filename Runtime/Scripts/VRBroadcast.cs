1721337316c502e00cf7fcce027ae8c11a6c3478
Vasanth Mohan <vmohan7@stanford.edu>
2021-08-11 20:50:03 -0700

    Testing VRCamStream and confirmed no bitrate targets sent

--- Runtime/Scripts/VRBroadcast.cs
+++ Runtime/Scripts/VRBroadcast.cs
@@ -115,6 +115,7 @@ namespace FusedVR.VRStreaming {
             string[] parameters = sdp.Split('\n'); //split on new line
             string customParam = parameters[parameters.Length - 2].Split('=')[1]; //get line before last new line and split on =
             string value = ""; //get dictionary value
+            Debug.LogError("TODO: add null check in case x is missing");
             JsonConvert.DeserializeObject<Dictionary<string, string>>(customParam).TryGetValue("user", out value);
             return value;
         }
---
+++ Runtime/Scripts/VRCamStream.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+using Unity.RenderStreaming;
+using Unity.WebRTC;
+
+namespace FusedVR.VRStreaming {
+    public class VRCamStream : CameraStreamer {
+        private string mainConnection = "";
+
+        // Start is called before the first frame update
+        void Start() {
+            OnStartedStream += StartStream;
+            StartCoroutine(TestBitrate());
+        }
+
+        private void StartStream(string connectionId) {
+            mainConnection = connectionId;
+        }
+
+        // Update is called once per frame
+        IEnumerator TestBitrate() {
+            while (true) {
+                if (Senders.TryGetValue(mainConnection, out var sender)) {
+                    RTCRtpSendParameters parameters = sender.GetParameters();
+                    foreach (var encoding in parameters.encodings) {
+                        Debug.Log("Min Bitrate : " + encoding.minBitrate);
+                        Debug.Log("Max Bitrate : " + encoding.maxBitrate);
+                    }
+                }
+
+                yield return new WaitForSeconds(2f);
+            }
+
+
+        }
+    }
+}
+
